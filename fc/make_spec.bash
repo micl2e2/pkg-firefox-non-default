3ce4f437a7646be1e8fc00434dd5ea443aecf3c2
Michael Lee <mail@micl2e2.com>
2026-05-03 16:43:28 +0800

    pkg name to non-default

--- fc/make_spec.bash
+++ fc/make_spec.bash
@@ -1,16 +1,16 @@
 
-cp firefox-non-release.template.spec firefox-non-release.spec
+cp firefox-non-default.template.spec firefox-non-default.spec
 
 VERSION_ESR=$(cat version.esr)
-sed -e "s/<V_ESR>/${VERSION_ESR}/" -i firefox-non-release.spec
+sed -e "s/<V_ESR>/${VERSION_ESR}/" -i firefox-non-default.spec
 
 VERSION_DEV=$(cat version.dev)
-sed -e "s/<V_DEV>/${VERSION_DEV}/" -i firefox-non-release.spec
+sed -e "s/<V_DEV>/${VERSION_DEV}/" -i firefox-non-default.spec
 
 VERSION_NGT=$(cat version.ngt)
-sed -e "s/<V_NGT>/${VERSION_NGT}/" -i firefox-non-release.spec
+sed -e "s/<V_NGT>/${VERSION_NGT}/" -i firefox-non-default.spec
 
 VERSION_REL=$(cat version.release_prefix )
-sed -e "s/<V_REL>/${VERSION_REL}/" -i firefox-non-release.spec
+sed -e "s/<V_REL>/${VERSION_REL}/" -i firefox-non-default.spec
 
 
--- fc/prepare_local_source.bash
+++ fc/prepare_local_source.bash
@@ -1,4 +1,4 @@
-DIR_NAME=firefox-non-release-local-sources
+DIR_NAME=firefox-non-default-local-sources
 tar --create --file $DIR_NAME.tar $DIR_NAME && cp $DIR_NAME.tar ~/rpmbuild/SOURCES/
 
 
